c680971dd5bd00fd03520426f135f130acdd0b58
Serhii Duniashenko <Flashbuster666@gmail.com>
2022-12-11 21:06:33 +0200

    Script changed

--- switchcase.js
+++ switchcase.js
@@ -1,17 +1,32 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr);
 
-switch(numOrStr) {
-    case !isNaN(numOrStr) || numOrStr :
-        console.log(' number is Ba_NaN');
+switch(true) {
+    case isNaN(+numOrStr) :
+        console.log('number is Ba_NaN');
     break;
-    case null :
+    case numOrStr === null :
         console.log('ви скасували');
     break;
-    case '' :
+    case numOrStr.trim() === '' :
         console.log('Empty String');
     break;
     default: 
     console.log('OK!')
     break;
 }
+
+// switch(numOrStr) {
+//     case !isNaN(numOrStr) || numOrStr :
+//         console.log(' number is Ba_NaN');
+//     break;
+//     case null :
+//         console.log('ви скасували');
+//     break;
+//     case '' :
+//         console.log('Empty String');
+//     break;
+//     default: 
+//     console.log('OK!')
+//     break;
+// }
